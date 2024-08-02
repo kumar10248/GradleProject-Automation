0d48514edb48deffc8eb23a99732b448df4d21ac
kumar10248 <kumardevashish000@gmail.com>
2024-08-03 01:37:34 +0530

    refactor: Remove commented code and unused imports

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -7,17 +7,12 @@ public class App{
 
 	public static void main(String[] args) throws InterruptedException {
 		
-//		System.setProperty("webdriver.chrome.driver", "/Users/pradeepsahu/Desktop/selenium\ WebDriver\ Jars/chromedriver-mac-arm64/chromedriver");
-		
-		
 		ChromeDriver driver = new ChromeDriver();
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		
 		driver.get("http://google.com");
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//*[@id=\"APjFqb\"]")).sendKeys("indiabix",Keys.ENTER);
 		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3")).click();
-		// js.executeScript("windows.scrollTo(0, document.body.scrollHeight)");
 		js.executeScript("window.scrollBy(0,350)", "");
 		driver.findElement(By.xpath("/html/body/main/section/div/div[2]/div[9]/div/div/ul/li[1]/a")).click();
 		driver.findElement(By.xpath("//*[@id=\"btnTheme\"]/i")).click();
